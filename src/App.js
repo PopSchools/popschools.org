2b3a8f7f1174875cc9037d51c17e724d67466e6e
unknown <tuganimana01@gmail.com>
2020-07-10 22:43:32 +0200

    adding navbar

--- src/App.js
+++ src/App.js
@@ -2,23 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './css/tailwind.css';
+import 'bulma/css/bulma.css'
+import Navbar from './components/Navbar.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p className="text-5xl font-extrabold">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+     
     </div>
   );
 }
