db4ddf142a4fe4cdb9c325da3bf95c76af82f07c
Nikema Prophet <nikema@Nikemas-MBP-2.attlocal.net>
2020-11-13 22:39:04 -0800

    Remove placeholders in preparation for publishing
    I removed some of the place holder content and links. I would like to publish this scaled-down version of the landing page and add elements back in when we have the actual data in place.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Logo from '../image/logo.png';
 import '../css/tailwind.css';
 import '../App.css';
-import {GithubFilled,TwitterCircleFilled,FacebookFilled  } from '@ant-design/icons';
+import {GithubFilled,TwitterCircleFilled } from '@ant-design/icons';
 // import 'bulma/css/bulma.css'
 function Footer(){
     return(
@@ -13,54 +13,20 @@ function Footer(){
 
                 <div className="flex flex-wrap ">
                     <div className="w-full md:w-1/3 ">
-                     <img src={Logo}  className="w-64 "/>
+                     <img src={Logo}  className="w-64 "alt="popschools" />
                     </div>
 
                     <div className="w-full md:w-2/3">
 
                         <div className="grid grid-cols-2 md:grid-cols-3">
-                            <div >
-                                <div  data-aos="fade-down"   data-aos-duration="1000">
-                                <p className="py-3 px-3 text-gray-600 font-bold">Essential links</p>
-                                <a href="" ><p className="px-3 text-gray-600 hover:text-gray-600 py-2">Popschool club</p></a>
-                                <a href="" ><p className="px-3 text-gray-600 hover:text-gray-600 py-2">Mutualaid.fund</p></a>
-                                <a href="" ><p className="px-3 text-gray-600 hover:text-gray-600 py-2">ForkThisRepo.com</p></a>
-                                <a href="" ><p className="px-3 text-gray-600 hover:text-gray-600 py-2">PopSchools.studio</p></a>
-                                </div>
-                                <div  data-aos="fade-down"   data-aos-duration="1000">
-                                <p className="pt-16 px-3 text-gray-600 font-bold">Developers</p>
-                                <a href="" ><p className="px-3 text-gray-600 hover:text-gray-600  py-2">Open source projects</p></a>
-                                <a href="" ><p className="px-3 text-gray-600 hover:text-gray-600 py-2">Webinars</p></a>
-                                <a href="" ><p className="px-3 text-gray-600 hover:text-gray-600 py-2">Communities</p></a>
-                                <a href="" ><p className="px-3 text-gray-600 hover:text-gray-600 py-2">Meetup</p></a>
-                                </div>
-                            </div>
-                            <div>
-                            <div  data-aos="fade-down"   data-aos-duration="1000">
-                                <p className="pt-3 px-3 text-gray-600 font-bold">Organization</p>
-
-                                <a href="" ><p className="px-3 text-gray-600 hover:text-gray-600 py-2">About us</p></a>
-                                <a href="" ><p className="px-3 text-gray-600 hover:text-gray-600 py-2">Sponsors</p></a>
-                                <a href="" ><p className="px-3 text-gray-600 hover:text-gray-600 py-2">Career</p></a>
-                                <a href="" ><p className="px-3 text-gray-600 hover:text-gray-600 py-2">Contact</p></a>
-                                <a href="" ><p className="px-3 text-gray-600 hover:text-gray-600 py-2">Team</p></a>
-                                </div>
-                                <div  data-aos="fade-down"   data-aos-duration="1000">
-                                <p className="pt-5 px-3 text-gray-600 font-bold">Resources</p>
-                                <a href="" ><p className="px-3 text-gray-600 hover:text-gray-600 py-2">Blog</p></a>
-                                <a href="" ><p className="px-3 text-gray-600 hover:text-gray-600 py-2">Webinars</p></a>
-                                <a href="" ><p className="px-3 text-gray-600 hover:text-gray-600 py-2">Career Guidance</p></a>
-                                <a href="" ><p className="px-3 text-gray-600 hover:text-gray-600 py-2">Meetup</p></a>
-                               </div>
-                            </div>
+                       
                             <div>
                                 <p className="py-3 px-3 text-gray-600 font-bold">On Social Media</p>
                                 <p className="text-gray-600 py-4 px-3">Follow us on our social media</p>
                                 <hr/>
 
                                <a href="https://github.com/PopSchools/popschools.org"><span className=" px-3 text-gray-600 text-4xl inline-block"><GithubFilled /></span> </a>
-                               <span className="px-3 text-gray-600 text-4xl inline-block"><TwitterCircleFilled /></span> 
-                               <span className="px-3 text-gray-600 text-4xl inline-block"><FacebookFilled /></span>
+                               <a href="https://twitter.com/popschoolsorg"><span className="px-3 text-gray-600 text-4xl inline-block"><TwitterCircleFilled /></span> </a>
                             </div>
 
                         </div>
@@ -69,12 +35,7 @@ function Footer(){
                             <span className="text-left text-gray-600 py-2 px-3"><span dangerouslySetInnerHTML={{ "__html": "&copy;" }} />{(new Date().getFullYear())} Popschools, Inc</span> 
                             </div>
 
-                            <div className="py-6">
-                            <a href="" ><p className="px-3 text-gray-600 hover:text-gray-600 inline-block mr-2">Privacy</p></a>
-                                <a href="" ><p className="px-3 text-gray-600 hover:text-gray-600  inline-block mr-2">Terms and Condition</p></a>
-                                
-                            </div>
-
+                         
                         </div>
 
 
